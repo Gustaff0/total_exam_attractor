c736ed0ead002ea2e00f46b5f0938fb3622c6a00
Gustaff0 <barsbekclash@gmail.com>
2021-08-21 18:43:26 +0600

    Накатил фикстуры, так же добавил поиск в лист представлении Обьявлении

--- announcement/webapp/forms.py
+++ announcement/webapp/forms.py
@@ -2,10 +2,19 @@ from django import forms
 from django.forms import widgets
 from django.utils.safestring import mark_safe
 
-from webapp.models import Announcement
+from webapp.models import Announcement, Comment
 
 
 class AnnouncementFormForUser(forms.ModelForm):
     class Meta:
         model = Announcement
         fields = ('title', 'descriptions', 'photo', 'category', 'price')
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment',)
+
+class SearchForm(forms.Form):
+    search_value = forms.CharField(max_length=100, required=False, label='Найти')
--- announcement/webapp/views/announcement.py
+++ announcement/webapp/views/announcement.py
@@ -1,8 +1,11 @@
 from django.core.exceptions import PermissionDenied
+from django.db.models import Q
 from django.shortcuts import redirect, render
+from django.utils.http import urlencode
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic.edit import FormMixin
 
-from webapp.forms import AnnouncementFormForUser
+from webapp.forms import AnnouncementFormForUser, SearchForm, CommentForm
 from webapp.models import Announcement
 
 
@@ -12,11 +15,35 @@ class AnnouncementListView(ListView):
     context_object_name = 'announcements'
     ordering = ('-created_at',)
 
+    def get(self, request, **kwargs):
+        self.form = SearchForm(request.GET)
+        self.search_data = self.get_search_data()
+        return super(AnnouncementListView, self).get(request, **kwargs)
+
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset()
         queryset = queryset.filter(moderate=True, delete=False)
+
+        if self.search_data:
+            queryset = queryset.filter(
+                Q(title__icontains=self.search_data) |
+                Q(descriptions__icontains=self.search_data), moderate=True, delete=False
+            )
         return queryset
 
+    def get_search_data(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search_value']
+        return None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = self.form
+
+        if self.search_data:
+            context['query'] = urlencode({'search_value': self.search_data})
+
+        return context
 
 class AnnouncementCreateView(CreateView):
     template_name = 'announcement/create.html'
@@ -64,6 +91,7 @@ class AnnouncementDetailView(DetailView):
     template_name = 'announcement/view.html'
     context_object_name = 'announcement'
     model = Announcement
+
 
 
 def announcement_delete_view(request, *args, **kwargs):
